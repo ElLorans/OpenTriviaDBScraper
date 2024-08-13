78b0cf0442eec174eff317f7f9c839796d3c1832
ElLorans <lorenzo.cerreta@gmail.com>
2024-08-13 09:28:06 +0200

    assign trivial pursuit category "macro_category"

--- open_trivia_db_scraper.py
+++ open_trivia_db_scraper.py
@@ -18,14 +18,14 @@ ERROR_CODES: Final[dict[int, str]] = {
     5: "Rate Limit: Too many requests have occurred. Each IP can only access the API once every 5 seconds.",
 }
 
-StrDictSequence = TypeVar('StrDictSequence', str, dict, Sequence)
+StrDictSequence = TypeVar("StrDictSequence", str, dict, Sequence)
 
 
 def unescape_any(obj: StrDictSequence) -> StrDictSequence:
     """
     Replace html-safe chars.
     :param obj: str | dict | Sequence
-    :return:
+    :return: same type of obj, after unescaping html chars
     """
     if isinstance(obj, dict):
         escaped = {}
@@ -37,7 +37,7 @@ def unescape_any(obj: StrDictSequence) -> StrDictSequence:
     if isinstance(obj, Sequence):
         return type(obj)(unescape_any(el) for el in obj)
 
-    warnings.warn(f'str | dict | Sequence expected, not {type(obj)}')
+    warnings.warn(f"str | dict | Sequence expected, not {type(obj)}")
     return obj
 
 
@@ -54,22 +54,27 @@ def fetch_trivia_questions(session: Session, token: str) -> dict:
     :param token: obtainable with get_new_token
     :return: dict
     """
-    response = session.get(f'https://opentdb.com/api.php?amount=50&token={token}')
+    response = session.get(f"https://opentdb.com/api.php?amount=50&token={token}")
     response.raise_for_status()  # Raise an error if the request failed
     json_data = response.json()
     if json_data["response_code"] != 0:
-        raise Exception(ERROR_CODES.get(json_data["error"], f"Unknown Error: Response {response.status_code} but"
-                                                            f"response_code: {json_data['response_code']}"))
-    return unescape_any(json_data['results'])
+        raise Exception(
+            ERROR_CODES.get(
+                json_data["error"],
+                f"Unknown Error: Response {response.status_code} but"
+                f"response_code: {json_data['response_code']}",
+            )
+        )
+    return unescape_any(json_data["results"])
 
 
-def read_db() -> dict:
+def read_db(path: str) -> dict:
     """
     Load dict from db.json in same folder. If not found, return empty dict.
     :return: dict
     """
     try:
-        with open('db.json') as f:
+        with open(path) as f:
             db = json.loads(f.read())
             print(f"Loaded {len(db)} questions.")
         return db
@@ -78,12 +83,42 @@ def read_db() -> dict:
         return {}
 
 
+def get_trivial_pursuit_cat(opendb_cat: str) -> str:
+    """
+    Convert opendb category to trivial pursuit category.
+    :param opendb_cat:
+    :return:
+    """
+    opendb_to_trivial: Final[dict[str, str]] = {
+        "Art": "Art and Literature",
+        "Animals": "Science and Nature",
+        "History": "History",
+        "Entertainment": "Entertainment",
+        "Sports": "Sports and Leisure",
+        "Politics": "History",
+        "Celebrities": "Entertainment",
+        "Mythology": "History",
+        "General Knowledge": "General Knowledge",  # This has no match, so might stay as is
+        "Science & Nature": "Science and Nature",
+        "Vehicles": "Science and Nature",
+        "Geography": "Geography",
+        "Science": "Science and Nature",
+    }
+
+    cat = opendb_cat.split(":", 1)[0]  # remove text after ":"
+    try:
+        return opendb_to_trivial[cat]
+    except KeyError:
+        warnings.warn(f"Unexpected category: {cat}")
+        return cat
+
+
 def main():
     """
     Load db.json if present, fetch question from opentdb.com and save to db.json and db.csv.
-    :return:
+    :return: None
     """
-    db = read_db()
+    db: dict = read_db("db.json")
     with Session() as session:
         token = get_new_token(session)
 
@@ -91,29 +126,42 @@ def main():
             while True:
                 results = fetch_trivia_questions(session, token)
                 for result in results:
-                    db[result['question']] = result
+                    macro_cat = get_trivial_pursuit_cat(result["category"])
+                    if macro_cat in db:
+                        db[macro_cat].append(result)
+                    else:
+                        db[macro_cat] = [result]
 
-                with open('db.json', 'w') as f:
-                    json.dump(db, f)
+                with open("db.json", "w") as file:
+                    json.dump(db, file)
 
                 try:
                     import pandas as pd
-                    df = pd.read_csv(db.values())
+
+                    flat_db = []
+                    for cat, questions in db.items():
+                        for question in questions:
+                            question["macro_category"] = cat
+                            flat_db.append(question)
+
+                    df = pd.DataFrame(flat_db)
                     df.to_csv("db.csv", index=False)
                 except ImportError:
                     print("pandas not installed, no csv will be saved.")
 
                 # Show progress
-                tqdm.write(f"Fetched {len(results)} questions. Total: {len(db)} questions saved.")
+                tqdm.write(
+                    f"Fetched {len(results)} questions. Total: {len(db)} questions saved."
+                )
                 time.sleep(5.5)
 
         except (KeyboardInterrupt, Exception) as e:
             print(e)
-            with open('db.json', 'w') as f:
+            with open("db.json", "w") as f:
                 json.dump(db, f, indent=4)
             print("Data saved. Exiting.")
 
 
-#%%
-if __name__ == '__main__':
+# %%
+if __name__ == "__main__":
     main()
